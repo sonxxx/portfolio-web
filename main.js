9a024273cfdcbac736b71290e97fd8c7df34778f
sonxxx <sh328sh@gmail.com>
2021-11-04 20:41:51 +0900

    JS: project filtering

--- main.js
+++ main.js
@@ -11,3 +11,83 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if(link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const contackBtn = document.querySelector('.home__contact');
+
+contackBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+
+// Make home slowly fade to transparent as the window scroll down
+const home= document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//  Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight/2){
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null){
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+  
+      if(filter === '*' || filter === project.dataset.type){
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+
+});
+
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+};
